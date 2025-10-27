b616ad7903609a951884462f433323d5184af76d
Drew Robinson <49618885+arobins222@users.noreply.github.com>
2025-10-27 14:48:10 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,33 @@ window.addEventListener('scroll', function () {
     header.classList.remove('shrink');
   }
 });
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+// Get elements
+const modal = document.getElementById('bookingModal');
+const btn = document.getElementById('bookBtn');
+const span = document.getElementsByClassName('close')[0];
+
+// Open modal
+btn.onclick = function (e) {
+  e.preventDefault(); // prevent default anchor action
+  modal.style.display = 'block';
+};
+
+// Close modal
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+// Close if clicked outside modal content
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
